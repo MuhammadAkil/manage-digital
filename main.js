51f00823792dac9a1ce382eafef716aa4533f8a8
Akil <akil.mehar@consoliads.com>
2022-09-08 11:08:17 +0500

    Fix navToggle

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
- 
+ const navToggle=document.querySelector(".mobile-nav-toggle");
+ const primaryNav=document.querySelector(".primary-navigation");    
+
+ navToggle.addEventListener('click',()=>{
+    primaryNav.hasAttribute('data-visible')
+    ?navToggle.setAttribute("aria-expanded",false)//Turnry operator
+    :navToggle.setAttribute("aria-expanded",true);
+primaryNav.toggleAttribute('data-visible');//primaryNav.classList.toggle('className'); Then called it in css and apply given properties position:fixed;display:block;
+ });        
